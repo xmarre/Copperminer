a4974bde773e4d55819be523c1bc9202ae208a29
xmarre <mmquant1@gmail.com>
2025-07-09 00:29:52 +0200

    Initial commit

--- start_gallery_ripper.bat
+++ start_gallery_ripper.bat
@@ -1,3 +1,12 @@
 @echo off
+REM Change to script directory (in case launched from elsewhere)
+cd /d "%~dp0"
+
+REM Activate virtual environment
+call .venv\Scripts\activate.bat
+
+REM Run your script
 python gallery_ripper.py
+
+REM Keep the window open so you can read any output
 pause
